4ed0d24fcf7556ec5c3f42c306f81c6d39128e85
Sajal <sajalkumarr030@gmail.com>
2024-06-09 02:24:41 +0530

    styling added

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -5,7 +5,7 @@ const containerStyle = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  height: "100vh", // This will center the button vertically in the viewport
+  height: "100vh",
 };
 
 const buttonStyle = {
@@ -21,28 +21,13 @@ const buttonStyle = {
   boxShadow: "0 2px 4px rgba(0,0,0,0.2)",
 };
 
-const iconStyle = {
-  marginRight: "10px",
-  padding: "5px",
-};
-
 const LoginButton = () => {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
-
   return (
     <div style={containerStyle}>
       {!isAuthenticated ? (
-        <button style={buttonStyle} onClick={() => loginWithRedirect()}>
-          <div style={iconStyle}>
-            <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google logo" width="20" height="20" />
-          </div>
-          Log In with Google
-        </button>
-      ) : (
-        <button style={buttonStyle} onClick={() => logout()}>
-          Log Out
-        </button>
-      )}
+        <button style={buttonStyle} onClick={() => loginWithRedirect()}>Log In with Google</button>) : (
+        <button style={buttonStyle} onClick={() => logout()}>Log Out</button>)}
     </div>
   );
 };
